b425ab173d6fd8a39e5da6c46cdb9758c17f10b3
LeonardoXavier1 <leonardomx500@gmail.com>
2023-04-18 01:41:16 -0300

    landing page

---
+++ script.js
@@ -0,0 +1,5 @@
+
+window.sr = ScrollReveal({reset: true}) ;
+
+sr.reveal('.cabecalho-imagem, .cabecalho-menu-item, .conteudo-principal-imagem, .conteudo-principal-escrito-titulo, .conteudo-principal-escrito-subtitulo, .conteudo-principal-escrito-botao, .conteudo-secundario-titulo, .conteudo-secundario-paragrafo, .rodape',{duration: 4000, rotate: {x:0, y:30, z:0} })
+
